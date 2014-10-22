64cc138e5047e4144d41048168520c13dd766676
miguelvaldes <miguel.valdes.s@gmail.com>
2014-10-22 16:51:31 -0400

    Se agrega proyecto interceptor de peticiones

---
+++ InspectorTest/ModuloHttp/Comportamiento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+
+namespace ModuloHttp
+{
+    public class Comportamiento : IHttpModule
+    {
+        private HttpApplication httpApp;
+
+        public void Init(HttpApplication httpApp)
+        {
+            this.httpApp = httpApp;
+            httpApp.BeginRequest += new EventHandler(OnBeginRequest);
+        }
+
+        void OnBeginRequest(object sender, EventArgs a)
+        {
+            HttpContext contexto = ((HttpApplication)sender).Context;
+            string filePath = contexto.Request.FilePath;
+            if (filePath.Equals("/About"))
+            {
+                // obtener data de la url
+                // contexto.Request.QueryString
+                
+                // obtener data del post
+                // contexto.Request.Form
+
+                // obtener data de sesion
+                // contexto.Session
+            }
+        }
+
+        public void Dispose() { }
+    }
+}
